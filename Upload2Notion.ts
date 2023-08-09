221cf62fcc7ceb0968a132de4330da9b6479414a
Jiaxin Peng <peng9498@gmail.com>
2023-08-09 20:15:47 +0100

    update the function of password and icon

--- Upload2Notion.ts
+++ Upload2Notion.ts
@@ -28,13 +28,13 @@ export class Upload2Notion {
 
 	// 因为需要解析notion的block进行对比，非常的麻烦，
 	// 暂时就直接删除，新建一个page
-	async updatePage(notionID:string, title:string, allowTags:boolean, tags:string[], type:string, slug:string, stats:string, category:string, summary:string, childArr:any) {
+	async updatePage(notionID:string, title:string, allowTags:boolean, tags:string[], type:string, slug:string, stats:string, category:string, summary:string, paword:string, favicon:string, childArr:any) {
 		await this.deletePage(notionID)
-		const res = await this.createPage(title, allowTags, tags, type, slug, stats, category, summary, childArr)
+		const res = await this.createPage(title, allowTags, tags, type, slug, stats, category, summary, paword, favicon, childArr)
 		return res
 	}
 
-	async createPage(title:string, allowTags:boolean, tags:string[], type:string, slug:string, stats:string, category:string, summary:string, childArr: any) {
+	async createPage(title:string, allowTags:boolean, tags:string[], type:string, slug:string, stats:string, category:string, summary:string, pawrod:string, favicon:string, childArr: any) {
 		const bodyString:any = {
 			parent: {
 				database_id: this.app.settings.databaseID
@@ -86,6 +86,24 @@ export class Upload2Notion {
 							}
 						}
 					]
+				},
+				password: {
+					rich_text: [
+						{
+							text: {
+								content: pawrod || ''
+							}
+						}
+					]
+				},
+				icon: {
+					rich_text: [
+						{
+							text: {
+								content: favicon || ''
+							}
+						}
+					]
 				}
 			},
 			children: childArr,
@@ -118,7 +136,7 @@ export class Upload2Notion {
 		}
 	}
 
-	async syncMarkdownToNotion(title:string, allowTags:boolean, tags:string[], type:string, slug:string, stats:string, category:string, summary:string, markdown: string, nowFile: TFile, app:App, settings:any): Promise<any> {
+	async syncMarkdownToNotion(title:string, allowTags:boolean, tags:string[], type:string, slug:string, stats:string, category:string, summary:string, paword:string, favicon:string, markdown: string, nowFile: TFile, app:App, settings:any): Promise<any> {
 		let res:any
 		const yamlObj:any = yamlFrontMatter.loadFront(markdown);
 		const __content = yamlObj.__content
@@ -127,9 +145,9 @@ export class Upload2Notion {
 		const notionID = frontmasster ? frontmasster.notionID : null
 
 		if(notionID){
-				res = await this.updatePage(notionID, title, allowTags, tags, type, slug, stats, category, summary, file2Block);
+				res = await this.updatePage(notionID, title, allowTags, tags, type, slug, stats, category, summary, paword, favicon, file2Block);
 		} else {
-			 	res = await this.createPage(title, allowTags, tags, type, slug, stats, category, summary, file2Block);
+				res = await this.createPage(title, allowTags, tags, type, slug, stats, category, summary, paword, favicon, file2Block);
 		}
 		if (res.status === 200) {
 			await this.updateYamlInfo(markdown, nowFile, res, app, settings)
